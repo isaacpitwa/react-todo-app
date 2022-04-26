abf115f33f8cb2a5f9f1603a30222b3a97660c45
isaacpitwa <isaacpitwa256@gmail.com>
2022-04-26 23:54:45 +0300

    Update readme

--- src/functionBased/pages/SinglePage.js
+++ src/functionBased/pages/SinglePage.js
@@ -13,7 +13,7 @@ function SinglePage() {
       slug: 'about-author',
       title: 'About the Author',
       description:
-            'This app was developed by Ibas Majid, a self-taught web developer and a technical writer. He is opened to freelance Gig. So go ahead and connect with ibas on Twitter @ibaslogic.',
+            'This app was developed by Isaac Pitwa, a self-taught web developer and a technical writer. He is opened to freelance Gig. So go ahead and connect with ibas on Twitter @isaacpitwa.',
     },
   ];
   const { slug } = useParams();
